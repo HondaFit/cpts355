2dfeed9a4f309a103b3ef91963679d7d7023c434
TaChonkerz <harish.s.sri@gmail.com>
2024-01-28 14:41:59 -0800

    learned math function

---
+++ tutorials/numbers.hs
@@ -0,0 +1,19 @@
+main = do
+    print (5+2)
+    print (7+9)
+    print (8*2)
+    print (4/4)
+
+    print(2**3) --2^3
+    print (3**3) --3^3
+
+    print (sqrt 36) --sqare root of 36
+
+    print (round 3.6) --rounds 3.6 so it would print 4 
+    print (round 3.3) --rounds 3.3 so it would print 3
+
+    print (floor 7.9) --floor would round down always 
+    print (ceiling 11.1) --ceiling would round up always 
+    
+
+
--- tutorials/variables.hs
+++ tutorials/variables.hs
@@ -1,33 +1,36 @@
-name3 :: String
-name3 = "bobby"
+import Data.Text.Lazy.Read (double)
+name3 :: String -- declaring name3 as string
+name3 = "bobby" -- assigning value
+
+numOfGifts2 :: Int -- declaring numofGifts2 as int
+numOfGifts2 = 5 -- assigning value 
+
+myLetter::Char
+myLetter = 'a'
+
+dudleyGpa :: Double
+dudleyGpa = 2.36
 
 main = do
-    let name = "Dudley"
-    putStrLn (name ++" was a bad child")
-    putStrLn (name ++" was a good child")
-    putStrLn (name ++" ate a lot of food")
-    putStrLn (name ++" got in trouble today. " ++ name ++ "is going to get scollded by his parents")
--- to comment use "--"
--- to have a coment block open with "{-" and end with "-}"
+  let name = "Dudley"
+  putStrLn (name ++ " was a bad child") --we're feeding the fuction (putStrln) the string. It is sperated by space
+  putStrLn (name ++ " was a good child")
+  putStrLn (name ++ " ate a lot of food")
+  putStrLn (name ++ " got in trouble today. " ++ name ++ "is going to get scollded by his parents")
+  -- to comment use "--"
+  -- to have a coment block open with "{-" and end with "-}"
 
-    --YOU CANNOT PRINT INTEGERS YOU NEED TO CONVERT THEM INTO STRINGS
-    print 36 --you cannot use putStrLn to print an integer use "print" instead
-    putStrLn (show 36) -- shows an integer as a string
+  -- YOU CANNOT PRINT INTEGERS YOU NEED TO CONVERT THEM INTO STRINGS
+  print 36 -- you cannot use putStrLn to print an integer use "print" instead
+  putStrLn (show 36) -- shows an integer as a string
+  let numOfGifts = 30
 
-    let numOfGifts = 30
+  putStrLn ("Today was " ++ name ++ " birthday he recieved " ++ show numOfGifts ++ " gifts") -- in order to print integers to the console you need to use "show"
+  let name2 = "fred"
 
-    putStrLn ("Today was "++name++" birthday he recieved "++show numOfGifts++ " gifts") -- in order to print integers to the console you need to use "show"
+  putStrLn (name2 ++ " and " ++ name ++ " got into a fight resulting in " ++ show numOfGifts ++ " deaths")
 
-    let name2 = "fred"
-
-    putStrLn (name2++ " and " ++name++ " got into a fight resulting in " ++show numOfGifts++ " deaths")
-
-    putStrLn (name3++ " and " ++name++ " got into a fight resulting in " ++show numOfGifts++ " deaths")
-
-    
-
-    
-
-
-    
-
+  putStrLn (name3 ++ " and " ++ name ++ " got into a fight resulting in " ++ show numOfGifts2 ++ " deaths")
+  
+  print myLetter --you cannot use putStrln, it is only using for string, print can be used for ints, and chars 
+  print dudleyGpa
