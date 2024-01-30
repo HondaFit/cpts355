9cb9eeb4772711ee8dd93abd6ef10a9ff5dbf358
Harish Sridharan <harish.s.sri@gmail.com>
2024-01-29 16:26:38 -0800

    learned new stuff

--- tutorials/userInput.hs
+++ tutorials/userInput.hs
@@ -1,4 +1,4 @@
 main = do 
     putStrLn "Please enter your name: "
     name <- getLine -- "<-" take the value of the right side and store it into name. "getline" gets input from user
-    putStrLn name
+    putStrLn ("Your name is: "++name++".")
